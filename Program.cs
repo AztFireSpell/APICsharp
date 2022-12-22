75b77dd12f10379bdd55fd283b3e4febed926976
AztFireSpell <alondigu@gmail.com>
2022-12-22 16:39:05 -0600

    Add swagger for documentation API

--- Program.cs
+++ Program.cs
@@ -1,5 +1,22 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pizza Store Api", Description = "Making the pizzas you love!", Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pizza StoreAPI V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
